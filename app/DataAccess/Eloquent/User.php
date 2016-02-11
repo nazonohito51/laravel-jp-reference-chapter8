c1d4db51a0fd802a91acee10c6597e11d479b0f4
nazonohito51 <nazonohito51@hotmail.com>
2016-02-11 15:56:26 +0900

    remove trait from User Model

--- app/DataAccess/Eloquent/User.php
+++ app/DataAccess/Eloquent/User.php
@@ -8,9 +8,9 @@ use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Model implements AuthenticatableContract, CanResetPasswordContract
+class User extends Model implements AuthenticatableContract
 {
-    use Authenticatable, CanResetPassword;
+    use Authenticatable;
 
     /**
      * The database table used by the model.
