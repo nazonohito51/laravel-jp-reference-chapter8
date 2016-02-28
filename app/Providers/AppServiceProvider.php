b70e2251582b5ca5c6de6be0991208ecf60f49f1
nazonohito51 <nazonohito51@hotmail.com>
2016-02-28 17:04:26 +0900

    add CustomValidator to ServiceContainer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Http\Validators\CustomValidator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app['validator']->resolver(function($translator, $data, $rules, $message) {
+            return new CustomValidator($translator, $data, $rules, $message);
+        });
     }
 
     /**
