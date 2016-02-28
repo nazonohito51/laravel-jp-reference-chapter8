4015d81baf6a051a241bfca14989f682a407a642
nazonohito51 <nazonohito51@hotmail.com>
2016-02-28 15:27:34 +0900

    refactoring AuthController

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
-use Validator;
+use App\Services\UserService;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRegisterRequest;
+use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
 class AuthController extends Controller
@@ -22,67 +23,28 @@ class AuthController extends Controller
 
     use AuthenticatesAndRegistersUsers;
 
+    /** @var Guard */
+    protected $auth;
+
     /**
-     * Create a new authentication controller instance.
-     *
-     * @return void
+     * @param Guard $auth
      */
-    public function __construct()
+    public function __construct(Guard $auth)
     {
         $this->middleware('guest', ['except' => 'getLogout']);
+        $this->auth = $auth;
     }
 
     /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
+     * @param UserRegisterRequest $request
+     * @param UserService $user
+     * @return \Illuminate\Http\RedirectResponse
      */
-    protected function validator(array $data)
+    public function postRegister(UserRegisterRequest $request, UserService $user)
     {
-        return Validator::make($data, [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|confirmed|min:6',
-        ]);
-    }
-
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
-    }
-
-    public function postRegister(Request $request)
-    {
-        $validator = $this->validator($request->all());
-        if ($validator->fails()) {
-            $this->throwValidationException(
-                $request, $validator
-            );
-        }
-
-        $user = $this->create($request->all());
-        \Mail::send(
-            'emails.register',
-            ['user' => $user],
-            function ($m) use ($user) {
-                $m->sender('nazonohito51@hotmail.com', 'Laravelリファレンス')
-                    ->to($user->email, $user->name)
-                    ->subject('ユーザー登録が完了しました');
-            }
-        );
-
-        \Auth::login($user);
-        return redirect($this->redirectPath());
+        $input = $request->only(['name', 'email', 'password']);
+        $result = $user->registerUser($input);
+        $this->auth->login($result);
+        return redirect()->route('admin.entry.index');
     }
 }
