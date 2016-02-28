4fb1aa6344c0b7b286365c679e350f325ac96cb3
nazonohito51 <nazonohito51@hotmail.com>
2016-02-28 15:35:24 +0900

    add CaptchaServiceProvider

---
+++ app/Providers/CaptchaServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use Gregwar\Captcha\CaptchaBuilder;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class CaptchaServiceProvider
+ * @package App\Providers
+ */
+class CaptchaServiceProvider extends ServiceProvider
+{
+    /** @var bool */
+    protected $defer = true;
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(
+            'Gregwar\Captcha\CaptchaBuilderInterface',
+            function() {
+                return new CaptchaBuilder();
+            }
+        );
+    }
+
+    public function provides()
+    {
+        return [
+            '\Gregwar\Captcha\CaptchaBuilderInterface',
+        ];
+    }
+}
