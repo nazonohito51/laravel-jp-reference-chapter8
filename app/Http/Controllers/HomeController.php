07b7e685d807990cf8b0c1bb74e95843ff27ed4c
nazonohito51 <nazonohito51@hotmail.com>
2016-03-06 17:13:14 +0900

    add HomeController for debug

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $captcha = app('Gregwar\Captcha\CaptchaBuilderInterface');
+        var_dump($captcha);
+        return 'test';
+    }
+}
