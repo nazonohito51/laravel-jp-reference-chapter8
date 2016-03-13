abd525ce16151dbfd3f9c29f2d04a9532818fef2
nazonohito51 <nazonohito51@hotmail.com>
2016-03-13 18:07:26 +0900

    fix Entry model

--- app/DataAccess/Eloquent/Entry.php
+++ app/DataAccess/Eloquent/Entry.php
@@ -1,10 +1,31 @@
 <?php
 
-namespace App\DataAccess\\Eloquent;
+namespace App\DataAccess\Eloquent;
 
 use Illuminate\Database\Eloquent\Model;
 
-class \Entry extends Model
+class Entry extends Model
 {
-    //
+    //use \App\DataAccess\Eloquent\SaveTransactionalTrait;
+
+    /** @var string */
+    protected $table = 'entries';
+
+    /** @var array */
+    protected $fillable = ['title', 'body', 'user_id'];
+
+    /**
+     * @param $limit
+     * @param $page
+     *
+     * @return mixed
+     */
+    public function byPage($limit, $page)
+    {
+        return $this->query()
+            ->orderBy('created_at', 'desc')
+            ->skip($limit * ($page - 1))
+            ->take($limit)
+            ->get();
+    }
 }
