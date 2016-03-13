68e7d192fa0e75861d53fc67187e8bb49762918c
nazonohito51 <nazonohito51@hotmail.com>
2016-03-13 16:40:36 +0900

    add cache method to EntryRepository

--- app/Repositories/EntryRepository.php
+++ app/Repositories/EntryRepository.php
@@ -6,14 +6,19 @@ use App\DataAccess\Eloquent\Entry;
 
 class EntryRepository implements EntryRepositoryInterface
 {
+    /** @var Cacheable */
+    protected $cache;
+
     /** @var Entry */
     protected $eloquent;
 
     /**
      * @param Entry $eloquent
+     * @param Cacheable $cache
      */
-    public function __construct(Entry $eloquent)
+    public function __construct(Entry $eloquent, Cacheable $cache)
     {
+        $this->cache = $cache;
         $this->eloquent = $eloquent;
     }
 
@@ -23,8 +28,69 @@ class EntryRepository implements EntryRepositoryInterface
      */
     public function save(array $params)
     {
+        $attributes = [];
         $attributes['id'] = (isset($params['id'])) ? $params['id'] : null;
         $result = $this->eloquent->updateOrCreate($attributes, $params);
+        if ($result) {
+            $this->cache->flush();
+        }
         return $result;
+    }
+
+    /**
+     * @param $id
+     *
+     * @return mixed
+     */
+    public function find($id)
+    {
+        $cacheKey = "entry:{$id}";
+        if ($this->cache->has($cacheKey)) {
+            return $this->cache->get($cacheKey);
+        }
+        $result = $this->eloquent->find($id);
+        if ($result) {
+            $this->cache->put($cacheKey, $result);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return int
+     */
+    public function count()
+    {
+        $key = 'entry_count';
+        if ($this->cache->has($key)) {
+            return $this->cache->get($key);
+        }
+        $result = $this->eloquent->count();
+        $this->cache->put($key, $result);
+
+        return $result;
+    }
+
+    /**
+     * @param int $page
+     * @param int $limit
+     *
+     * @return \stdClass
+     */
+    public function byPage($page = 1, $limit = 20)
+    {
+        $key = "entry_page:{$page}:{$limit}";
+        if ($this->cache->has($key)) {
+            return $this->cache->get($key);
+        }
+        $entries = $this->eloquent->byPage($limit, $page);
+
+        return $this->cache->putPaginateCache(
+            $page,
+            $limit,
+            $this->count(),
+            $entries,
+            $key
+        );
     }
 }
