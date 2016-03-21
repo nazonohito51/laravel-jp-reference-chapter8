373061ed3038ad325129a2f784d9994e449b6876
nazonohito51 <nazonohito51@hotmail.com>
2016-03-21 17:54:46 +0900

    add AuthorizableContractInterface to UserModel

--- app/DataAccess/Eloquent/User.php
+++ app/DataAccess/Eloquent/User.php
@@ -4,15 +4,20 @@ namespace App\DataAccess\Eloquent;
 
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
-class User extends Model implements AuthenticatableContract
+class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
     use Authenticatable;
 
     use SaveTransactionalTrait;
+
+    // 5.1.11で追加されたAuthorizationを利用します
+    use Authorizable;
 
     /**
      * The database table used by the model.
@@ -26,7 +31,7 @@ class User extends Model implements AuthenticatableContract
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['name', 'email', 'password', 'auth_token'];
 
     /**
      * The attributes excluded from the model's JSON form.
@@ -34,4 +39,15 @@ class User extends Model implements AuthenticatableContract
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+    /**
+     *
+     * @param  string $value
+     *
+     * @return string
+     */
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
 }
