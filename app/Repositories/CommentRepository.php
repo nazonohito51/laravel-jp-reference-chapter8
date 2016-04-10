4d4f3606a4470cc8047fb5738f870489de0e8391
nazonohito51 <nazonohito51@hotmail.com>
2016-04-10 17:12:16 +0900

    add Repository test

--- app/Repositories/CommentRepository.php
+++ app/Repositories/CommentRepository.php
@@ -2,36 +2,57 @@
 
 namespace App\Repositories;
 
+use App\DataAccess\Cache\Cacheable;
 use App\DataAccess\Eloquent\Comment;
 
+/**
+ * Class CommentRepository
+ */
 class CommentRepository implements CommentRepositoryInterface
 {
-    /** @var Commnet */
+    /** @var Cacheable */
+    protected $cache;
+
+    /** @var Comment */
     protected $eloquent;
 
     /**
-     * @param Comment $eloquent
+     * @param Comment   $eloquent
+     * @param Cacheable $cache
      */
-    public function __construct(Comment $eloquent)
+    public function __construct(Comment $eloquent, Cacheable $cache)
     {
         $this->eloquent = $eloquent;
+        $this->cache = $cache;
     }
 
     /**
      * @param $id
+     *
      * @return mixed
      */
     public function allByEntry($id)
     {
-        return $this->eloquent->getAllByEntryId($id);
+        $cacheKey = "comments:{$id}";
+        if ($this->cache->has($cacheKey)) {
+            return $this->cache->get($cacheKey);
+        }
+        $result = $this->eloquent->getAllByEntryId($id);
+        if ($result) {
+            $this->cache->put($cacheKey, $result);
+        }
+        return $result;
     }
 
     /**
      * @param array $params
+     *
      * @return mixed
      */
     public function save(array $params)
     {
-        return $this->eloquent->fill($params)->save();
+        $result = $this->eloquent->fill($params)->save();
+        $this->cache->flush();
+        return $result;
     }
 }
